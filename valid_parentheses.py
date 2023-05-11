635f716fcc6c00b733ccdabce9bb836ccfe7dde3
Nemsiss Shahbazian <nemsisss@usc.edu>
2023-05-10 17:37:29 -0700

    May 10 p1

---
+++ valid_parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for i in s:
+            if i=='(' or i=='[' or i=='{':
+                stack.append(i)
+            else:
+                if len(stack)==0:
+                    return False
+                elif (i==']' and stack[len(stack)-1]!='[') or (i=='}' and stack[len(stack)-1]!='{') or (i==')' and stack[len(stack)-1]!='('):
+                    return False
+                stack.pop()
+        
+        return not len(stack)
