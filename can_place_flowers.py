c09af204ee5941ad2de7067411bb4ce7f2c30dcf
Nemsiss Shahbazian <nemsisss@usc.edu>
2023-06-07 12:51:33 -0700

    *** Jun7 p41 ***

---
+++ can_place_flowers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+
+        for i in range(len(flowerbed)):
+            if flowerbed[i]==0:
+                leftEmpty= (i==0) or (flowerbed[i-1]==0)
+                rightEmpty= (i==len(flowerbed)-1) or (flowerbed[i+1]==0)
+                if leftEmpty and rightEmpty:
+                    flowerbed[i]=1
+                    n-=1
+        return n<=0
+
+
+    
