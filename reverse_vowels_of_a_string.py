41dcc75255e6c6437a106342b32d1e5ad0b70e4d
Nemsiss Shahbazian <nemsisss@usc.edu>
2024-03-15 18:40:00 -0700

    p39 [UPDATED: added JS]

--- reverse_vowels_of_a_string.py
+++ reverse_vowels_of_a_string.py
@@ -12,3 +12,29 @@ class Solution:
             s[i]=new_dict[i]
 
         return ''.join(s)
+########## Javascript ##########
+    # let vowels= new Set(['a', 'e', 'i', 'o','u', 'A', 'E', 'I', 'O', 'U']);
+    # let str= [...s];
+    # let left=0;
+    # let right=s.length-1;
+    
+    # while(left<right)
+    # {
+    #     if(!vowels.has(str[left]))
+    #     {
+    #         left+=1;
+    #     }
+    #     if(!vowels.has(str[right]))
+    #     {
+    #         right-=1;
+    #     }
+    #     if(vowels.has(str[right]) && vowels.has(str[left]))
+    #     {
+    #         let temp=str[left];
+    #         str[left]=str[right];
+    #         str[right]=temp;
+    #         left+=1;
+    #         right-=1;
+    #     }
+    # }
+    # return str.join('');
